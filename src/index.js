82dada202bbc4578ef6b35c7fb45f23f0a12be1a
Mariano Alvarez <malvarezh92@gmail.com>
2016-11-23 15:17:15 -0600

    create table and logic to add rows

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 import './index.css';
-import numeral from 'numeral';
 
-const courseValue = numeral(1000).format('$0,0.00')
-console.log(`I would pay ${courseValue} fro this awesome course`);
+import { getUsers, deleteUser } from './api/userApi';
+
+// Populate table
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody += `
+      <tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+      </tr>
+    `
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const el = event.target;
+      event.preventDefault();
+      deleteUser(el.attributes['data-id'].value);
+      const row = el.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    }
+  })
+
+});
+
